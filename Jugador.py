35d1734a0c8f68581855594161b515523d23a91d
unknown <codicegamer@gmail.com>
2021-08-25 20:41:19 -0300

    se tertmina el proyecto y se empieza la correccion de errores

--- Jugador.py
+++ Jugador.py
@@ -2,10 +2,11 @@ from tablero import Tablero
 
 class Jugador: 
     
-    def __init__(self, ancho, alto):
-        self.nombre = None
+    def __init__(self, ancho, alto, orden):
+        self.nombre = ""
         self.ancho_tablero = ancho
         self.alto_tablero = alto
+        self.identificarse(orden)
         self.tablero = Tablero(ancho, alto)
     
     def barcos_randoms(self):
@@ -22,7 +23,7 @@ class Jugador:
                 except:
                     print ("La fila ingresada no existe.")
                     continue
-                if posicion_x.isnumeric() and posicion_x =< self.ancho_tablero and posicion_x != 0 and posicion_y =< self.alto_tablero:
+                if posicion_x.isnumeric() and posicion_x <= self.ancho_tablero and posicion_x != 0 and posicion_y <= self.alto_tablero:
                     verificador = True
                 else:
                     print("La columna ingresada no existe")
@@ -30,23 +31,29 @@ class Jugador:
         
     def atacar(self):
         verificador = False
-            while not verificador:
-                posicion_x = input("Ingrese la columna(1, 2, 3...): ")
-                posicion_y = input("Ingrese la fila(A, B, C...): ")
-                try:
-                    posicion_y = letras_a_numeros[posicion_y.upper]
-                except:
-                    print ("La fila ingresada no existe.")
-                    continue
-                if posicion_x.isnumeric() and posicion_x =< self.ancho_tablero and posicion_x != 0 and posicion_y =< self.alto_tablero:
-                    verificador = True
-                else:
-                    print("La columna ingresada no existe")
-            self.tablero.lanzar(int(posicion_x) - 1, posicion_y - 1)
-            hay_barcos = self.tablero.verificar_barcos()
-            return hay_barcos
+        while not verificador:
+            posicion_x = input("Ingrese la columna(1, 2, 3...): ")
+            posicion_y = input("Ingrese la fila(A, B, C...): ")
+            try:
+                posicion_y = letras_a_numeros[posicion_y.upper()]
+            except:
+                print ("La fila ingresada no existe.")
+                continue
+            if posicion_x.isnumeric() and posicion_x <= self.ancho_tablero and posicion_x != 0 and posicion_y <= self.alto_tablero:
+                verificador = True
+            else:
+                print("La columna ingresada no existe")
+        self.tablero.lanzar(int(posicion_x) - 1, posicion_y - 1)
+        hay_barcos = self.tablero.verificar_barcos()
+        return hay_barcos
 
-    
+    def identificarse(self, orden):
+        if int(orden) == 1:
+            self.nombre = input("ingresen el nombre del primer jugador   ")
+        else:
+            self.nombre = input("ingresen el nombre del segundo jugador   ")
+
+
     letras_a_numeros = {
     "A" : 1,
     "B" : 2,
--- juego.py
+++ juego.py
@@ -5,22 +5,65 @@ class Juego:
     def __init__(self):
         self.jugador1 = None
         self.jugador2 = None
+        self.juego_nuevo()
+        self.cambiar_turno()
 
     def juego_nuevo(self):
         verificador = False
-        while not verificador
+        while not verificador:
             ancho = input("Decidan, el ancho de los tableros: ")
             alto = input("Decidan, el alto de los tableros: ")
-            if ancho.isnumeric() and alto.isnumeric() and ancho < 28 and alto < 28:
+            if ancho.isnumeric() and alto.isnumeric() and int(ancho) < 28 and int(alto) < 28 and int(alto) * int(ancho) > 7 :
                 verificador = True
                 ancho = int(ancho)
                 alto = int(alto)
-        self.jugador1 = Jugador(ancho, alto)
-        self.jugador2 = Jugador(ancho, alto)
+            else:
+                print("el minimo del tablero es 7 casillas (4*2, 2*4 o 3*3)")
+        self.jugador1 = Jugador(ancho, alto, 1)
+        while True:
+            respuesta = input(self.jugador1.nombre + ". Desea colocar los barcos manualmente? (y/n)   ")
+            if respuesta.lower() in ("y", "n"):
+                if respuesta == "y":
+                    self.jugador1.colocar_barcos()
+                    break
+                else:
+                    self.jugador1.barcos_randoms()
+                    break
+            else:
+                print ("Ingrese un respuesta valida porfavor")
+        self.jugador2 = Jugador(ancho, alto, 2)
+        while True:
+            respuesta = input(self.jugador2.nombre + ". Desea colocar los barcos manualmente? (y/n)   ")
+            if respuesta.lower() in ("y", "n"):
+                if respuesta == "y":
+                    self.jugador2.colocar_barcos()
+                    break
+                else:
+                    self.jugador2.barcos_randoms()
+                    break
+            else:
+                print ("Ingrese un respuesta valida porfavor")
 
 
     def cambiar_turno(self):
-        gano = False
-        while not gano:
-            termino = jugador1.
-        
+        while True:
+            print("Turno jugador1")
+            termino = self.jugador2.atacar()
+            if not termino:
+                print ("El jugador 1 ha ganado")
+                break
+            print ("Turno jugador2")
+            termino = self.jugador1.atacar()
+            if not termino:
+                print ("El jugador 2 ha ganado")
+                break
+        while True:
+            respuesta = input("Desea empezar un juego nuevo? (y/n)")
+            if respuesta.lower in ('y', 'n'):
+                if respuesta.lower == 'y':
+                    self.juego_nuevo()
+                    break
+                else:
+                    break
+                
+j = Juego()
